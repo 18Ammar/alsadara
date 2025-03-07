2f5dd42fb4927d8ba8c35b1e460f364e64f65414
Ammar <Ammar@gmail.com>
2025-03-07 11:45:04 +0300

    creating InvalidRequest Exception Class

--- shared/exceptions.py
+++ shared/exceptions.py
@@ -1,74 +1,12 @@
 class BaseException(Exception):
     code = 500
-    def __init__(self, message:dict):
+    def __init__(self,message:dict):
         super().__init__(message.get("en") if isinstance(message,dict) else message)
-        self.msg=message.get("en") if isinstance(message,dict) else message
-        self.msg_ar=message.get("ar") if isinstance(message,dict) else message
+        self.msg = message.get("en") if isinstance(message,dict) else message
+        self.msg_ar = message.get("ar") if isinstance(message,dict) else message
+
 
 class InvalidRequest(BaseException):
-    def __init__(self, *args: object) -> None:
+    def __init__(self, *args:object)->None:
         super().__init__(*args)
-        self.code=400
-
-class NotAuthenticated(BaseException):
-    def __init__(self, *args: object) -> None:
-        super().__init__(*args)
-        self.code=401
-
-class NotLoggedIn(NotAuthenticated):
-    pass
-
-class InvalidToken(NotAuthenticated):
-    pass
-
-class MissingToken(NotAuthenticated):
-    pass
-
-class RevokedToken(NotAuthenticated):
-    pass
-
-class ExpiredToken(NotAuthenticated):
-    pass
-
-class InvalidSubject(NotAuthenticated):
-    pass
-
-class InvalidSignature(NotAuthenticated):
-    pass
-
-class Forbidden(BaseException):
-    def __init__(self, *args: object) -> None:
-        super().__init__(*args)
-        self.code=403
-
-class UnauthorizedAccount(Forbidden):
-    pass
-
-class OperationNotPermitted(Forbidden):
-    pass
-
-class NotFound(BaseException):
-    def __init__(self, *args: object) -> None:
-        super().__init__(*args)
-        self.code=404
-
-class MethodNotAllowed(BaseException):
-    def __init__(self, *args: object) -> None:
-        super().__init__(*args)
-        self.code=405
-
-class ValidationError(BaseException):
-    def __init__(self, *args: object) -> None:
-        super().__init__(*args)
-        self.code=406
-
-class NotAcceptable(ValidationError):
-    pass
-
-class NotUniqueError(BaseException):
-    def __init__(self, *args: object) -> None:
-        super().__init__(*args)
-        self.code=409
-
-class AlreadyExists(NotUniqueError):
-    pass
+        self.code = 400
